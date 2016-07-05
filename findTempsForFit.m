190fd02024b96a777529676ed7468a658921c29b
juppiega <juppiega@gmail.com>
2016-07-05 14:45:11 +0300

    Smaller bugfix

--- findTempsForFit.m
+++ findTempsForFit.m
@@ -5,7 +5,8 @@ Tex_est = evalTex(varStruct, coeff(TexStruct.coeffInd));
 
 T0 = clamp(200, evalT0(varStruct, T0Coeffs), 1000);
 dT0 = clamp(1, evalDT(varStruct, dTCoeffs), 30);
-varStruct.numBiases = origNumBiases;
 Tex = clamp(T0+1, Tex_est, 5000);
+
+varStruct.numBiases = origNumBiases;
 
 end
