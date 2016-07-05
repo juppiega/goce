e88b555fe6d3664974a6d0c342d8e6daea948fd5
juppiega <juppiega@gmail.com>
2016-07-05 10:23:01 +0300

    Add AR,O2

--- computeRho.m
+++ computeRho.m
@@ -1,4 +1,4 @@
-function [rho, OnumDens, N2numDens, HeNumDens] = computeRho(T0, dT0, Tex, Z, OlbDens, N2lbDens, HelbDens)
+function [rho, OnumDens, N2numDens, HeNumDens] = computeRho(T0, dT0, Tex, Z, OlbDens, N2lbDens, HelbDens, ArlbDens, O2lbDens)
 %global modelLbHeight
 sigma = dT0 ./ (Tex - T0);
 g = 9.418;
@@ -19,6 +19,14 @@ gamma_He = 4 * u2kg * g ./ (sigma*1E-3 .* k .* Tex);
 f_He = (T0 ./ T).^(1+gamma_He) .* exp(-sigma .* (Z) .* gamma_He);
 HeNumDens = HelbDens.*f_He; % [1/cm^3]
 
-rho = (16*OnumDens + 28*N2numDens + 4*HeNumDens) * u2kg * 1E6; % [kg/m^3]
+gamma_Ar = 40 * u2kg * g ./ (sigma*1E-3 .* k .* Tex);
+f_Ar = (T0 ./ T).^(1+gamma_Ar) .* exp(-sigma .* (Z) .* gamma_Ar);
+ArNumDens = ArlbDens.*f_Ar; % [1/cm^3]
+
+gamma_O2 = 32 * u2kg * g ./ (sigma*1E-3 .* k .* Tex);
+f_O2 = (T0 ./ T).^(1+gamma_O2) .* exp(-sigma .* (Z) .* gamma_O2);
+O2NumDens = O2lbDens.*f_O2; % [1/cm^3]
+
+rho = (16*OnumDens + 28*N2numDens + 4*HeNumDens + 40*ArNumDens + 32*O2NumDens) * u2kg * 1E6; % [kg/m^3]
 
 end
