fb993dc7f5f9109e968b19cc85ffc6e9d4a8efd8
juppiega <juppiega@gmail.com>
2017-01-05 12:02:16 +0200

    saflkj

--- removeDataPoints.m
+++ removeDataPoints.m
@@ -33,15 +33,32 @@ if nargin > 2
     end
     if fixSatIndices
         satInd = zeros(1, length(removeInd));
-        satInd(fixStruct.goce) = 1;
-        satInd(fixStruct.champ) = 2;
-        satInd(fixStruct.grace) = 3;
-        satInd(fixStruct.swarm) = 4;
+        
+        if isfield(fixStruct,'goce') satInd(fixStruct.goce) = 1; end
+        if isfield(fixStruct,'champ') satInd(fixStruct.champ) = 2; end
+        if isfield(fixStruct,'grace') satInd(fixStruct.grace) = 3; end
+        if isfield(fixStruct,'swarm') satInd(fixStruct.swarm) = 4; end
+        if isfield(fixStruct,'de2') satInd(fixStruct.de2) = 5; end
+        if isfield(fixStruct,'aeC') satInd(fixStruct.aeC) = 6; end
+        if isfield(fixStruct,'aeE') satInd(fixStruct.aeE) = 7; end
+        if isfield(fixStruct,'aeENace') satInd(fixStruct.aeENace) = 8; end
+        if isfield(fixStruct,'aeEOss') satInd(fixStruct.aeEOss) = 9; end
+        if isfield(fixStruct,'guvi') satInd(fixStruct.guvi) = 10; end
+        if isfield(fixStruct,'aeros') satInd(fixStruct.aeros) = 11; end
+        if isfield(fixStruct,'aeCOss') satInd(fixStruct.aeCOss) = 12; end
         satInd(removeInd) = [];
-        fixStruct.goce = find(satInd == 1);
-        fixStruct.champ = find(satInd == 2);
-        fixStruct.grace = find(satInd == 3);
-        fixStruct.swarm = find(satInd == 4);
+        if isfield(fixStruct,'goce') fixStruct.goce = find(satInd == 1); end
+        if isfield(fixStruct,'champ') fixStruct.champ = find(satInd == 2); end
+        if isfield(fixStruct,'grace') fixStruct.grace = find(satInd == 3); end
+        if isfield(fixStruct,'swarm') fixStruct.swarm = find(satInd == 4); end
+        if isfield(fixStruct,'de2') fixStruct.de2 = find(satInd == 5); end
+        if isfield(fixStruct,'aeC') fixStruct.aeC = find(satInd == 6); end
+        if isfield(fixStruct,'aeE') fixStruct.aeE = find(satInd == 7); end
+        if isfield(fixStruct,'aeENace') fixStruct.aeENace = find(satInd == 8); end
+        if isfield(fixStruct,'aeEOss') fixStruct.aeEOss = find(satInd == 9); end
+        if isfield(fixStruct,'guvi') fixStruct.guvi = find(satInd == 10); end
+        if isfield(fixStruct,'aeros') fixStruct.aeros = find(satInd == 11); end
+        if isfield(fixStruct,'aeCOss') fixStruct.aeCOss = find(satInd == 12); end
     end
     if fixZ
         fixStruct.Z(removeInd) = [];
--- subsampleStructs.m
+++ subsampleStructs.m
@@ -6,30 +6,30 @@ subsamp = round(1 / (subsampPercent / 100));
 
 N = length(rhoStruct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-rhoStruct = removeDataPoints(rhoStruct, removeInd, false, true, false, false);
+rhoStruct = removeDataPoints(rhoStruct, removeInd, true, true, true, true);
 
 N = length(TexStruct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-TexStruct = removeDataPoints(TexStruct, removeInd);
+TexStruct = removeDataPoints(TexStruct, removeInd, true, true, true, true);
 
 N = length(OStruct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-OStruct = removeDataPoints(OStruct, removeInd);
+OStruct = removeDataPoints(OStruct, removeInd, true, true, true, true);
 
 N = length(N2Struct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-N2Struct = removeDataPoints(N2Struct, removeInd);
+N2Struct = removeDataPoints(N2Struct, removeInd, true, true, true, true);
 
 N = length(HeStruct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-HeStruct = removeDataPoints(HeStruct, removeInd);
+HeStruct = removeDataPoints(HeStruct, removeInd, true, true, true, true);
 
 N = length(ArStruct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-ArStruct = removeDataPoints(ArStruct, removeInd);
+ArStruct = removeDataPoints(ArStruct, removeInd, true, true, true, true);
 
 N = length(O2Struct.data);
 removeInd = ~ismember(1:N, 1:subsamp:N);
-O2Struct = removeDataPoints(O2Struct, removeInd);
+O2Struct = removeDataPoints(O2Struct, removeInd, true, true, true, true);
 
 end
