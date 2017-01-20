02aab4376d54a20a27e1f0e518d664449a11a729
juppiega <juppiega@gmail.com>
2017-01-20 14:04:36 +0200

    Change tols

--- fitIlModel.m
+++ fitIlModel.m
@@ -590,7 +590,7 @@ quietInd = [TexStruct.coeffInd(1:numQuietCoeffs),...
 stormInd = setdiff(1:length(initGuess), quietInd);
 
 tolX = 1E-8;
-tolFun = 1E-7;
+tolFun = 1E-6;
 tolOpt = 1E-4;
 lambda0 = 1E-2;
 if quietData
