0312f441923c88d49110bfdce2da2ea6a57b1374
juppiega <juppiega@gmail.com>
2017-02-22 09:54:13 +0200

    Download newer ISR

--- downloadRadarData.m
+++ downloadRadarData.m
@@ -5,8 +5,8 @@ MILLSTONE_HILL = 30;
 ARECIBO = 20;
 inst = [EISCAT, MILLSTONE_HILL, ARECIBO];
 instStr = {'EISCAT', 'MILLSTONE_HILL', 'ARECIBO'};
-beginDatenum = datenum('1996-01-01');
-endDatenum = datenum('2008-12-31');
+beginDatenum = datenum('2009-01-01');
+endDatenum = datenum('2016-12-31');
 
 tic;
 for i = 1:length(inst)
