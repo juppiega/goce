c8c31e84ce8b7d5b05a4dcda5550a41fc7cf5ddd
juppiega <juppiega@gmail.com>
2017-03-21 15:25:40 +0200

    dT vis

--- visualizeFit.m
+++ visualizeFit.m
@@ -60,7 +60,7 @@ latitudeMean = false;
 devFromXmean = false;
 sameColorBars = false;
 plotSurfs(z, lat, lst, lon, doy, F, FA, aeInt, Ap, lstMean, lonMean, latitudeMean, devFromXmean, ...
-    sameColorBars, 'yx', 'rho', coeffStruct, numBiasesStruct);
+    sameColorBars, 'yx', 'dT', coeffStruct, numBiasesStruct);
 
 if exist('msisDtmComparison.mat', 'file')
     load msisDtmComparison.mat
