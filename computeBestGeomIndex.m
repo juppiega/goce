e5e8ce2cc2d624e60087fa2195c2d01134ab842d
juppiega <juppiega@gmail.com>
2017-06-09 11:26:43 +0300

    asdflöjk

--- computeBestGeomIndex.m
+++ computeBestGeomIndex.m
@@ -10,10 +10,12 @@ elseif strcmpi(satellite,'champ')
     removeInd = ~ismember(1:N, rhoStruct.champ); scaleFac = 0.9578;
 elseif strcmpi(satellite,'grace')
     removeInd = ~ismember(1:N, rhoStruct.grace); scaleFac = 0.9362;
+elseif strcmpi(satellite,'all')
+    removeInd = false(N,1); scaleFac = 0.9034;
 end
 
 rhoStruct = removeDataPoints(rhoStruct, removeInd, false, true, false, false);
-[rhoStruct] = removeAndFixData(rhoStruct, 0, [], [], [], [], [], [], [], [], false);
+%[rhoStruct] = removeAndFixData(rhoStruct, 0, [], [], [], [], [], [], [],[], false); %orbAver
 
 numBiasesStruct = struct('O', 5, 'N2', 6,...
      'He', 5, 'Ar', 2, 'O2', 0);
@@ -30,16 +32,36 @@ coeffStruct = struct('TexCoeff' , optCoeff(TexInd),...
 
 aeInt = rhoStruct.aeInt;
 rhoStruct.aeInt = zeros(size(rhoStruct.aeInt))+20;
-[quietRho] = computeComparisonData(rhoStruct, coeffStruct, numBiasesStruct);
-quietRho = quietRho * scaleFac;
+[modelRho] = computeComparisonData(rhoStruct, coeffStruct, numBiasesStruct);
+modelRho = modelRho * scaleFac;
+[modelRho, times] = computeOrbitAverage(modelRho, rhoStruct.latitude, rhoStruct.timestamps);%orbAver
 
-rhoStruct.aeInt = aeInt;
+% orbAver
+aeIntAver = zeros(length(times),size(aeInt,2));
+for i = 1:size(aeInt,2)
+    aeIntAver(:,i) = computeOrbitAverage(aeInt(:,i), rhoStruct.latitude, rhoStruct.timestamps);
+end
 
-rhoDiff = rhoStruct.data ./ quietRho - 1;
+obsRho = computeOrbitAverage(rhoStruct.data, rhoStruct.latitude, rhoStruct.timestamps); % orbAver
 
-aeInt(rhoDiff < 0,:) = [];
+% orbAver
+quietInd = all(aeIntAver < 500, 2);
+modelRho(quietInd) = [];
+aeIntAver(quietInd,:) = [];
+obsRho(quietInd) = [];
+times(quietInd) = [];
+
+rhoDiff = obsRho ./ modelRho - 1;
+
+aeIntAver(rhoDiff < 0,:) = [];
+times(rhoDiff < 0) = [];
 rhoDiff(rhoDiff < 0) = [];
 
-mdl = stepwiselm(aeInt, rhoDiff, 'upper','quadratic');
+mdl = stepwiselm(aeIntAver, rhoDiff, 'upper','quadratic','intercept',true,'PEnter',0.33,'PRemove',0.5)
+predDiff = predict(mdl,aeIntAver);
+figure;
+plot(times,predDiff, times,rhoDiff);
+legend('pred','obs')
+datetick('x')
 
 end
