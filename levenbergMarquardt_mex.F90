c5e883b81bd14aa354c6c133887d99df49954713
juppiega <juppiega@gmail.com>
2017-06-12 14:10:47 +0300

    fjklö

--- levenbergMarquardt_mex.F90
+++ levenbergMarquardt_mex.F90
@@ -552,10 +552,12 @@ function G_storm(a, S)
 
     k = 0;
 
-    Activity = a(k+1)*S%aeInt(:,5) + a(k+2)*S%aeInt(:,7) + &
-           a(k+3)*S%aeInt(:,2)**2 + a(k+4)*S%aeInt(:,3)**2 + &
-           a(k+5)*S%aeInt(:,4)**2 + a(k+6)*S%aeInt(:,7)**2;
-    k = k + 6;
+    Activity = a(k+1)*S%aeInt(:,1) + a(k+2)*S%aeInt(:,3) + a(k+3)*S%aeInt(:,4)  + &
+            a(k+4)*S%aeInt(:,5) + a(k+5)*S%aeInt(:,7) + &
+            a(k+6)*S%aeInt(:,3)*S%aeInt(:,4) + a(k+7)*S%aeInt(:,3)*S%aeInt(:,5) + &
+            a(k+8)*S%aeInt(:,5)*S%aeInt(:,7) + a(k+9)*S%aeInt(:,3)**2 + &
+            (k+10)*S%aeInt(:,4)**2 + a(k+11)*S%aeInt(:,7)**2;
+    k = k + 11;
 
     mag_lat = (a(k+1)+a(k+2)*S%P20+a(k+3)*S%P40 + (a(k+4)*S%P10+a(k+5)*S%P30)*(1+a(k+6)*S%FA+a(k+7)*S%FA**2)*cos(S%dv-a(k+8))+&
                a(k+9)*S%FA+a(k+10)*S%FA**2) * Activity;
