f2aacee1f9e8dd99f7764ab6f7196c13855a69dc
juppiega <juppiega@gmail.com>
2017-06-13 10:30:05 +0300

    säadflöj

--- G_storm.m
+++ G_storm.m
@@ -14,7 +14,7 @@ Activity = a(k+1)*S.aeInt(:,1) + a(k+2)*S.aeInt(:,3) + a(k+3)*S.aeInt(:,4)  + ..
             (k+10)*S.aeInt(:,4).^2 + a(k+11)*S.aeInt(:,7).^2;
 k = k + 11;
 
-mag_lat = (a(k+1)+a(k+2)*S.P20+a(k+3)*S.P40 + (a(k+4)*S.P10+a(k+5)*S.P30).*(1+a(k+6)*S.FA+a(k+7)*S.FA.^2).*cos(S.dv-a(k+8))+...
+mag_lat = (a(k+1)+a(k+2)*S.P20+a(k+3)*S.P40 + (a(k+4)*S.P10+a(k+5)*S.P30).*(1+a(k+6)*S.FA+a(k+7)*S.FA.^2).*cos(S.yv-a(k+8))+...
            a(k+9)*S.FA+a(k+10)*S.FA.^2) .* Activity;
 k = k + 10;
 
