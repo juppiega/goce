562ebd44c1cea0fd5cf155ecd616516d2cb4b585
juho <juho@laptop.Princeton.EDU>
2017-11-30 13:18:35 -0500

    asd;oj

--- plotOC.m
+++ plotOC.m
@@ -5,7 +5,7 @@ load('quietIndRhoStruct.mat')
 
 rhoStruct = removeAndFixData(rhoStruct,0);
 oc_il = rhoStruct.data(quietInd) ./ ilRho(quietInd);
-oc_dtm = rhoStructdata(quietInd) ./ dtmRho(quietInd);
+oc_dtm = rhoStruct.data(quietInd) ./ dtmRho(quietInd);
 
 N = length(oc_il);
 plot(1:N,oc_il, 1:N,oc_dtm, 1:N,rhoStruct.aeInt(quietInd,7));
