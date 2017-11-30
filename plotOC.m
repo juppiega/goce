a96dfcc725d840b1d17ed509e1165598f201cbe4
juho <juho@laptop.Princeton.EDU>
2017-11-30 13:09:31 -0500

    asldj

---
+++ plotOC.m
@@ -0,0 +1,11 @@
+load('ilData.mat','rhoStruct')
+load('ilComparison.mat')
+load('msisDtmComparison.mat')
+load('quietIndRhoStruct.mat')
+
+rhoStruct = removeAndFixData(rhoStruct,0);
+oc_il = rhoStruct.data(quietInd) ./ ilRho(quietInd);
+oc_dtm = rhoStructdata(quietInd) ./ dtmRho(quietInd);
+
+N = length(oc_il);
+plot(1:N,oc_il, 1:N,oc_dtm, 1:N,rhoStruct.aeInt(quietInd,7));
