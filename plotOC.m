d1ac337cce1c3268a14c60c9387fb2b6bbc771a9
juho <juho@laptop.Princeton.EDU>
2017-11-30 13:26:38 -0500

    asd

--- plotOC.m
+++ plotOC.m
@@ -8,4 +8,4 @@ oc_il = rhoStruct.data(quietInd) ./ ilRho(quietInd);
 oc_dtm = rhoStruct.data(quietInd) ./ dtmRho(quietInd);
 
 N = length(oc_il);
-plot(1:N,oc_il, 1:N,oc_dtm, 1:N,rhoStruct.aeInt(quietInd,7));
+plot(1:N,oc_il, 1:N,oc_dtm, 1:N,rhoStruct.aeInt(quietInd,7)/200);
