190f1bad65166f0889d48e6465f4a12918e4c70f
juho <juho@laptop.Princeton.EDU>
2017-12-10 18:51:25 -0500

    asd

--- levenbergMarquardt_mex.F90
+++ levenbergMarquardt_mex.F90
@@ -1138,7 +1138,7 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
     !tolOpt = 1E3
     !lambda0 = 1E-2
     maxFuncEvals = 5000 * size(initGuess)
-    maxIter = 10000 !!!!!!!!!!!!!!!!!!!!!!
+    maxIter = 100 !!!!!!!!!!!!!!!!!!!!!!
 
     !funVec = modelMinimizationFunction(initGuess)
     
