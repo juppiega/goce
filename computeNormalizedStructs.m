d5d68bf8189b8524085874a2593f8e91d2cc6206
juho <juho@laptop.Princeton.EDU>
2017-12-19 15:17:08 -0500

    ;a;sdk

--- computeNormalizedStructs.m
+++ computeNormalizedStructs.m
@@ -18,4 +18,6 @@ normalizedData = computeNormalizedDensity(S.data, S.Z, name, Tex, dT, T0);
 
 save([name,'Normalized.mat'],'normalizedData');
 
+plot(S.timestamps, log(normalizedData), S.timestamps, S.aeInt/400 + 23);
+
 end
