1ff3caa70aaf7b6380c3f4e1a0eceeae39fa6336
juho <juho@laptop.Princeton.EDU>
2018-01-01 17:40:10 -0500

    lkasjd

--- computeBiases.m
+++ computeBiases.m
@@ -17,21 +17,47 @@ end
 rmInd = setdiff(1:length(S.data),ind);
 S = removeDataPoints(S, rmInd,true,true,true,true);
 
+load optCoeff
+
+if ~strcmpi(name, 'Tex') && ~strcmpi(name, 'T0') && ~strcmpi(name, 'dT')
+    [Tex, dT0, T0] = findTempsForFit_this(S, optCoeff(TexInd), dTCoeffs, T0Coeffs);
+    OlbDens = evalMajorSpecies(S, optCoeff(OInd), 5);
+    N2lbDens = evalMajorSpecies(S, optCoeff(N2Ind), 6);
+    HelbDens = evalMajorSpecies(S, optCoeff(HeInd), 5);
+    %ArlbDens = evalMajorSpecies(S, coeffStruct.ArCoeff, numBiasesStruct.Ar);
+    O2lbDens = exp(optCoeff(O2Ind));
+end
+
 if strcmpi(name,'O')
     [~,~,obs_dtm] = computeDtm(S);
     [~,~,obs_msis] = computeMsis(S);
+    [~,obs_il] = computeRho(T0, dT0, Tex, S.Z, OlbDens, N2lbDens, HelbDens, 0, O2lbDens);
 elseif strcmpi(name,'N2')
     [~,~,~,obs_dtm] = computeDtm(S);
     [~,~,~,obs_msis] = computeMsis(S);
+    [~,~,obs_il] = computeRho(T0, dT0, Tex, S.Z, OlbDens, N2lbDens, HelbDens, 0, O2lbDens);
 elseif strcmpi(name,'He')
     [~,~,~,~,obs_dtm] = computeDtm(S);
     [~,~,~,~,obs_msis] = computeMsis(S);
+    [~,~,~,obs_il] = computeRho(T0, dT0, Tex, S.Z, OlbDens, N2lbDens, HelbDens, 0, O2lbDens);
 elseif strcmpi(name,'Tex')
     [obs_dtm] = computeDtm(S);
     [obs_msis] = computeMsis(S);
+    obs_il = evalTex(S, optCoeff(TexInd));
 end
 
-OM_dtm = mean(obs_dtm./S.data)
-OM_msis = mean(obs_msis./S.data)
+logOM_dtm = log(mean(obs_dtm./S.data))
+logOM_msis = log(mean(obs_msis./S.data))
+logOM_il = log(mean(obs_il./S.data))
+
+end
+
+function [Tex, dT0, T0] = findTempsForFit_this(varStruct, TexCoeffs, dTCoeffs, T0Coeffs)
+
+Tex_est = evalTex(varStruct, TexCoeffs);
+
+T0 = clamp(200, evalT0(varStruct, T0Coeffs), 1000);
+dT0 = clamp(1, evalDT(varStruct, dTCoeffs), 30);
+Tex = clamp(T0+1, Tex_est, 5000);
 
 end
