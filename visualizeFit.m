3516092b2a7b91adf2bd9c5db167d59d3649c3b1
juho <juho@laptop.Princeton.EDU>
2018-02-23 12:48:44 -0500

    akljsd

--- visualizeFit.m
+++ visualizeFit.m
@@ -75,7 +75,7 @@ lonMean = false;
 latitudeMean = false;
 devFromXmean = false;
 sameColorBars = false;
-onlyIL = true;
+onlyIL = false;
 outputNetCdf = true;
 deviationFromQuiet = false;
 plotSurfs(z, lat, lst, lon, doy, F, FA, aeInt, Ap, lstMean, lonMean, latitudeMean, devFromXmean, ...
