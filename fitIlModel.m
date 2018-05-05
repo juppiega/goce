afa16981d928fdaaefb2edbfd95e7996cf608e41
juho <juho@laptop.Princeton.EDU>
2018-05-04 22:15:38 -0400

    asd

--- fitIlModel.m
+++ fitIlModel.m
@@ -1098,7 +1098,7 @@ if fitSimultaneously || fitBaseAgain
           allBiasInd = [Obiases, N2biases, HeBiases];
           paramsToFit = setdiff(paramsToFit,[Obiases, N2biases, HeBiases]);
           quietInd = setdiff(quietInd,[Obiases, N2biases, HeBiases]);
-          paramsToFit(O2Struct.coeffInd) = [];
+          paramsToFit(end) = [];
           ptfOrig = paramsToFit;
           
           %expTimes = [6.0546   17.9177    5.2326    3.9629];
