0dece7f34d5487e73ee92ae72c35168fa72debf3
juho <juho@laptop.Princeton.EDU>
2019-01-05 15:53:35 -0500

    Fit all params

--- fitIlModel.m
+++ fitIlModel.m
@@ -1116,8 +1116,8 @@ if fitSimultaneously || fitBaseAgain
         %initGuess(stormInd) = 0; % TESTAUS
         fun = @(coeff)modelMinimizationFunction(TexStruct, OStruct, N2Struct, HeStruct, ArStruct, O2Struct, rhoStruct, dTCoeffs, T0Coeffs, weights, tolX, coeff, paramsToFit);
         [comp] = fun(initGuess);
-        %tic;[optCoeff, JTWJ] = levenbergMarquardt_mex(TexStruct, OStruct, N2Struct, HeStruct, ArStruct, O2Struct, rhoStruct, dTCoeffs, T0Coeffs, weights, initGuess, paramsToFit, tolX, tolFun, tolOpt, lambda0, minLambda);toc;
-        load('coeffsAll.noSemiAn.mat','optCoeff','JTWJ')
+        tic;[optCoeff, JTWJ] = levenbergMarquardt_mex(TexStruct, OStruct, N2Struct, HeStruct, ArStruct, O2Struct, rhoStruct, dTCoeffs, T0Coeffs, weights, initGuess, paramsToFit, tolX, tolFun, tolOpt, lambda0, minLambda);toc;
+        %load('coeffsAll.noSemiAn.mat','optCoeff','JTWJ')
         %paramsToFit = ptfOrig; % Testaus
     else
          
